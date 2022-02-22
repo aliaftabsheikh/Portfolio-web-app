e4a4ffaa5f55521582afdacdfa5c1a4bdfdc279a
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-22 12:28:29 +0500

    header

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./Header.scss";
 import { motion } from "framer-motion";
-
 import { images } from "../../constants";
+
+import "./Header.scss";
+
+const scaleVariants = {
+  whileInView :{
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration : 1,
+      ease : 'easeInOut'
+    }
+  }
+}
 
 const Header = () => {
   return (
@@ -43,8 +54,16 @@ const Header = () => {
         />
       </motion.div>
 
-      <motion.div>
-        
+      <motion.div
+      variants={scaleVariants}
+      whileInView = {scaleVariants.whileInView}
+      className = 'app__header-circles'
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index)=>(
+          <div className="circle-cmp app__flex" key = {`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
       </motion.div>
     </div>
   );
