4e9fb8f39e70cb9a181aad3972e8dd13492e8368
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-23 11:53:26 +0500

    Update About.jsx

--- frontend_react/src/containers/About/About.jsx
+++ frontend_react/src/containers/About/About.jsx
@@ -4,8 +4,25 @@ import { images } from "../../constants";
 
 import "./About.scss";
 
+const abouts = [
+  {title : 'Web Development', description : 'I am a good web developer.', imgUrl : ''},
+  {title : 'Web Design', description : 'I am a good web designer.', imgUrl : ''},
+  {title : 'UI/UX', description : 'I am a good UI/UX Designer .', imgUrl : ''},   
+  {title : 'Web Animations', description : 'I am a Web Animations designer .', imgUrl : ''}   
+]
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+    <h2 className="head-text">
+      I know that
+      <span>Good Design</span>
+      <br/>
+      means
+      <span>Good Business</span>
+    </h2>
+    </>
+  )
 };
 
 export default About;
