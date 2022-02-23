0cf8c2548f879a6a8dddb0af88850e54049dc649
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-23 12:03:25 +0500

    About

--- frontend_react/src/containers/About/About.jsx
+++ frontend_react/src/containers/About/About.jsx
@@ -5,10 +5,10 @@ import { images } from "../../constants";
 import "./About.scss";
 
 const abouts = [
-  {title : 'Web Development', description : 'I am a good web developer.', imgUrl : ''},
-  {title : 'Web Design', description : 'I am a good web designer.', imgUrl : ''},
-  {title : 'UI/UX', description : 'I am a good UI/UX Designer .', imgUrl : ''},   
-  {title : 'Web Animations', description : 'I am a Web Animations designer .', imgUrl : ''}   
+  {title : 'Web Development', description : 'I am a good web developer.', imgUrl : images.about01},
+  {title : 'Web Design', description : 'I am a good web designer.', imgUrl :  images.about02},
+  {title : 'UI/UX', description : 'I am a good UI/UX Designer .', imgUrl :  images.about03},   
+  {title : 'Web Animations', description : 'I am a Web Animations designer .', imgUrl :  images.about04}   
 ]
 
 const About = () => {
@@ -21,6 +21,23 @@ const About = () => {
       means
       <span>Good Business</span>
     </h2>
+
+    <div className="app__profiles">
+      {abouts.map((about,index)=>(
+        <motion.div
+        whileInView={{opacity : 1}}
+        whileHover={{scale : 1.1}}
+        transition={{duration: 0.5, type: "tween"}}
+        className="app__profile-item"
+        key={about.title + index}
+        >
+          <img src={about.imgUrl} alt={about.title} />
+          <h2 className="bold-text" style={{marginTop : 20 }}>{about.title}</h2>
+          <p className="p-text" style={{marginTop : 10 }}>{about.description}</p>
+
+        </motion.div>
+      ))}
+    </div>
     </>
   )
 };
