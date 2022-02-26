1dc677cc07be25c6df0c77c6a0112786ee77008d
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-26 23:40:20 +0500

    work

--- frontend_react/src/containers/Work/Work.jsx
+++ frontend_react/src/containers/Work/Work.jsx
@@ -46,7 +46,9 @@ const Work = () => {
           >
             {filterWork.map((work, index)=>(
               <div className="app__work-item app__flex" key={index}>
-                <div className="app__work-img app__flex"></div>
+                <div className="app__work-img app__flex">
+                  <img src={urlFor(work.imgUrl )} alt={work.name} />
+                </div>
               </div>
             ))}
           </motion.div>
