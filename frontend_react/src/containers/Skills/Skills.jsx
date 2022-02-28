5e6a020190e73362493de3fd1021d32d5070d2c7
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-28 15:27:34 +0500

    skills & experience

--- frontend_react/src/containers/Skills/Skills.jsx
+++ frontend_react/src/containers/Skills/Skills.jsx
@@ -30,15 +30,23 @@ const Skills = () => {
 
     <div className="app__skills-container">
       <motion.div className="app__skills-list">
-        {skills.map((skills)=>(
-          <motion.div>
-            
+        {skills.map((skill)=>(
+          <motion.div
+          whileInView={{opacity: [0,1]}}
+          transition={{duration : 0.5}}
+          className='app__skills-item app__flex'
+          key={skill.name}
+          >
+            <div className="app__flex" style={{backgroundColor : skill.bgColor}}>
+              <img src={urlFor(skill.icon)} alt={skill.name} />
+            </div>
+            <p className="p-text">{skill.name}</p>
           </motion.div>
         ))}
       </motion.div>
     </div>
     </>
-  )
+  ) 
 }
 
 export default Skills
