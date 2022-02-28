e57ce44c3ebb114251c0e919cde5f2bb4045a989
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-28 15:18:51 +0500

    skills & experience

--- frontend_react/src/containers/Skills/Skills.jsx
+++ frontend_react/src/containers/Skills/Skills.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import ReactTooltip from 'react-tooltip'
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import "./Skills.scss"
 
 const Skills = () => {
+
+  const [experience, setExperience] = useState([])
+  const [skills, setSkills] = useState([])
+  
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query)
+    .then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery)
+    .then((data) => {
+      setSkills(data);
+    });
+  }, []);
   return (
-    <div>Skills</div>
+    <>
+    <h2 className="head-text">Skills & Experience</h2>
+
+    <div className="app__skills-container">
+      <motion.div className="app__skills-list">
+        {skills.map((skills)=>(
+          <motion.div>
+            
+          </motion.div>
+        ))}
+      </motion.div>
+    </div>
+    </>
   )
 }
 
