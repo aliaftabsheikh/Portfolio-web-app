e75f033c3407da912009d9fcc1f9e09d87e06287
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-01 20:04:05 +0500

    testimonials

--- frontend_react/src/containers/Testimonial/Testimonial.jsx
+++ frontend_react/src/containers/Testimonial/Testimonial.jsx
@@ -10,17 +10,21 @@ const Testimonial = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const handleClick = (index)=>{
+    setCurrentIndex(index)
+  }
+
   useEffect(() => {
     const query = '*[_type == "testimonials"]';
     const brandsQuery = '*[_type == "brands"]';
 
     client.fetch(query).then((data) => {
       setTestimonials(data);
-    })
+    });
 
     client.fetch(brandsQuery).then((data) => {
       setBrands(data);
-    })
+    });
   }, []); 
 
   const test = testimonials[currentIndex];
@@ -39,7 +43,7 @@ const Testimonial = () => {
         </div>
       </div>
 
-      <div className="app__testimonuals-btns app__flex">
+      <div className="app__testimonials-btns app__flex">
         <div className="app__flex" onClick={()=> handleClick(currentIndex === 0 ? testimonials.length -1 : currentIndex -1)}>
           <HiChevronLeft/>
         </div>
@@ -49,9 +53,24 @@ const Testimonial = () => {
       </div>
       </>
     )}
+
+    <div className="app__testimonials-brands app__flex">
+      {brands.map((brand)=>(
+        <motion.div
+        whileInView={{opacity:[0,1] }}
+        transition={{duration : 0.5, type : 'tween' }}
+        key={brand._id}
+        >
+          <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+        </motion.div>
+      ))}
+    </div>
+    
     </>
-  )
-}
+
+
+  );
+};
 
 // export default Testimonial
 
