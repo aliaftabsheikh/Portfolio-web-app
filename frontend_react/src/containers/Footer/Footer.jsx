2b503ce4cdea80766ef052a7800a84ebc2b134b6
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-02 11:20:13 +0500

    Footer

--- frontend_react/src/containers/Footer/Footer.jsx
+++ frontend_react/src/containers/Footer/Footer.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { client } from '../../client'
+import { images } from '../../constants'
+import { AppWrap, MotionWrap } from '../../wrapper'
+
 import "./Footer.scss"
 
 
@@ -8,4 +12,4 @@ const Footer = () => {
   )
 }
 
-export default Footer
+export default AppWrap(MotionWrap(Footer, 'app__footer'), 'contact', 'app__whitebg')
